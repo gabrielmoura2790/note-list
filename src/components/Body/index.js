f9d5afb47383a5290d003c06ff67593625bc8e10
gabrielmoura <gabrielmoura.br@gmail.com>
2021-09-27 18:47:06 -0300

    Adicionando funçao de deletar anotaçao

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -6,9 +6,11 @@ import {
 import firestore from '@react-native-firebase/firestore';
 
 import NoteList from '../NoteList';
+import { ActivityIndicator, View, Text, Alert } from 'react-native';
 
 export default function Body() {
     const [note, setNote] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const response = firestore()
@@ -22,6 +24,7 @@ export default function Body() {
                         id: doc.id,
                         ...doc.data(),
                     }
+                    setLoading(false);
                     setNote(oldArray => [...oldArray, list]);
                 });
             })
@@ -30,13 +33,49 @@ export default function Body() {
 
     }, []);
 
+    function handleDelete(data) {
+        Alert.alert(
+            'ATENÇÃO!',
+            `Você realmente deseja excluir a anotação: ${data.content} ?`,
+            [
+                {
+                    text: 'Cancelar',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Excluir',
+                    onPress: () => handleDeleteSuccess(data)
+                }
+            ]
+        )
+    }
+
+    async function handleDeleteSuccess(data) {
+        await firestore().collection('notes').doc(data.id).delete()
+    }
+
     return (
         <Container>
-            <List
-                showsVerticalScrollIndicator={false}
-                data={note}
-                renderItem={({ item }) => <NoteList data={item} />}
-            />
+            {
+                loading ? (
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        {
+                            note ? (
+                                <Text>Nenhuma nota adicionada...</Text>
+                            ) : (
+                                <ActivityIndicator size="large" color="#20e809" />
+                            )
+                        }
+                    </View>
+                ) :
+                    (
+                        <List
+                            showsVerticalScrollIndicator={false}
+                            data={note}
+                            renderItem={({ item }) => <NoteList data={item} deleteItem={handleDelete} />}
+                        />
+                    )
+            }
         </Container>
     );
 }
--- src/components/NoteList/index.js
+++ src/components/NoteList/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Container, Text, MarkButton, Ratio } from './styles';
+import { View } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
-export default function NoteList({data}) {
+export default function NoteList({ data, deleteItem }) {
 
   async function toggleSelect(id, select) {
 
     await firestore().collection('notes')
-    .doc(id).update({
-      select: !select,
-    })
+      .doc(id).update({
+        select: !select,
+      })
   }
 
   return (
-    <Container>
+    <Container onLongPress={() => deleteItem(data)} activeOpacity={1}>
       <Text
         numberOfLines={2}
         style={{ textDecorationLine: data.select ? "line-through" : "none", color: data.select ? "#808080" : "#000" }}>
@@ -34,7 +35,7 @@ export default function NoteList({data}) {
                 />
               ) :
               (
-                <Container />
+                <View />
               )
           }
 
